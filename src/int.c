04dc01f4594b0b00047b05cc78cc80db9f97266a
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-10-11 13:33:49 +0300

    Update int.c

--- src/int.c
+++ src/int.c
@@ -5,7 +5,7 @@
 int digitSum(int num) 
 {
         int digit, sum;
-        while (n > 0) {
+        while (num > 0) {
                 digit = num % 10;
                 sum += digit;
                 num /= 10;
